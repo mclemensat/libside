99f5347deeecac277da7495eb5a0f4b8efbdccfc
Joffrey Chevalier <chevalier.joffrey@live.fr>
2022-06-29 17:46:34 +0200

    create searchBar feature

--- libside/src/pages/homepage.jsx
+++ libside/src/pages/homepage.jsx
@@ -1,8 +1,11 @@
+import SearchBar from "../components/SearchBar";
+
 function HomePage() {
   return (
     <div className="text-center text-lg text-yellow-700 font-bold">
-      <h1>HELLOW</h1>
-      <p>Let's code !!</p>
+      <div>
+        <SearchBar />
+      </div>
     </div>
   );
 }
