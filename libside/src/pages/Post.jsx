88e5e1917b3e823afda997221029c06a75131232
Marie Clémensat <marieclemensat@wanadoo.fr>
2022-06-29 18:43:32 +0200

    wip

--- libside/src/pages/Post.jsx
+++ libside/src/pages/Post.jsx
@@ -22,13 +22,29 @@ export default function Post() {
 
   return (
     <div>
-      <div class="flex items-center justify-center p-12">
-        <div class="mx-auto w-full max-w-[550px]">
+      <div className="flex items-center justify-center p-12">
+        <div className="mx-auto w-full max-w-[550px]">
           <form onSubmit={handleSubmit}>
-            <div class="mb-5">
+          <div className="text-black mr-6">
+          <label htmlFor="persona" className="text-xl">
+            En tant que
+          </label>
+          <select
+            className="leading-tight drop-shadow-lg bg-[#D9D9D9] text-gray-400 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded-lg focus:outline-none focus:bg-slate-100 focus:border-gray-200"
+            id="persona"
+            name="persona"
+            value={newPost.articleType}
+            onChange={handleChange}
+          >
+            <option value="">PERSONA A SELECT.</option>
+            <option value="News">News</option>
+            <option value="Search">Search</option>
+          </select>
+        </div>
+            <div className="mb-5">
               <label
                 htmlFor="title"
-                class="mb-3 block text-base font-medium text-[#07074D]"
+                className="mb-3 block text-base font-medium text-[#07074D]"
               >
                 Title
               </label>
@@ -39,13 +55,13 @@ export default function Post() {
                 placeholder="Title"
                 value={newPost.title}
                 onChange={handleChange}
-                class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
               />
             </div>
-            <div class="mb-5">
+            <div className="mb-5">
               <label
                 htmlFor="body"
-                class="mb-3 block text-base font-medium text-[#07074D]"
+                className="mb-3 block text-base font-medium text-[#07074D]"
               >
                 Body
               </label>
@@ -56,13 +72,13 @@ export default function Post() {
                 placeholder="Article"
                 value={newPost.body}
                 onChange={handleChange}
-                class="w-full resize-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                className="w-full resize-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
               ></textarea>
             </div>
-            <div class="mb-5">
+            <div className="mb-5">
               <label
                 htmlFor="author"
-                class="mb-3 block text-base font-medium text-[#07074D]"
+                className="mb-3 block text-base font-medium text-[#07074D]"
               >
                 Author
               </label>
@@ -73,11 +89,11 @@ export default function Post() {
                 placeholder="Author"
                 value={newPost.author}
                 onChange={handleChange}
-                class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
               />
             </div>
             <div>
-              <button class="hover:shadow-form rounded-md bg-[#527CA4] py-3 px-8 text-base font-semibold text-white outline-none">
+              <button className="hover:shadow-form rounded-md bg-primary hover:bg-secondary py-3 px-8 text-base font-semibold text-white outline-none">
                 Submit
               </button>
             </div>
@@ -90,6 +106,7 @@ export default function Post() {
         <ul>
           {submittedPosts.map((post, index) => (
             <li key={index}>
+              {post.articleType}
               {post.title}
               {post.body}
               {post.author}
