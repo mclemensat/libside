9e8575773606a38e774ffa317903751d59d81e7c
Joffrey Chevalier <chevalier.joffrey@live.fr>
2022-06-30 16:24:49 +0200

    change newsboard

--- libside/src/pages/homepage.jsx
+++ libside/src/pages/homepage.jsx
@@ -1,14 +1,12 @@
-import Nav from "@components/Nav";
-import News from "@components/News";
 import SearchBar from "@components/SearchBar";
 import CardTown from "@components/CardTown.jsx";
 import PostComponent from "@components/PostComponent";
+import NewsBoard from "@components/NewsBoard";
 
 function HomePage() {
   return (
     <div className="">
-      <div>
-      </div>
+      <div></div>
       <div className="flex flex-around p-10 w-full">
         <div className="py-20  ">
           <SearchBar />
@@ -17,7 +15,7 @@ function HomePage() {
         </div>
 
         <div className="md:w-1/4 md:right-0 md:absolute">
-          <News />
+          <NewsBoard />
         </div>
       </div>
     </div>
