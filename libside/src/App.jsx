3f7e7e32cc63d6c63b5f2bec072011c0c1b5b472
CarolinaDelerue <delerue.carolina@gmail.com>
2022-06-30 12:31:10 +0200

    V1 componentDetails

--- libside/src/App.jsx
+++ libside/src/App.jsx
@@ -2,13 +2,15 @@ import { Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage";
 import Post from "./pages/Post";
+import ComponentDetails from "./pages/ComponentDetails";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/post" element={<Post />}/>
+        <Route path="/post" element={<Post />} />
+        <Route path="/component-details" element={<ComponentDetails />} />
       </Routes>
     </div>
   );
