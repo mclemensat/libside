3952c2212f816624050219daa8caf095636573dd
Joffrey Chevalier <chevalier.joffrey@live.fr>
2022-06-30 16:12:48 +0200

    wip

--- libside/src/pages/homepage.jsx
+++ libside/src/pages/homepage.jsx
@@ -2,6 +2,7 @@ import Nav from "@components/Nav";
 import News from "@components/News";
 import SearchBar from "@components/SearchBar";
 import CardTown from "@components/CardTown.jsx";
+import PostComponent from "@components/PostComponent";
 
 function HomePage() {
   return (
@@ -12,6 +13,7 @@ function HomePage() {
       <div className="flex flex-around p-10 w-full">
         <div className="py-20  ">
           <SearchBar />
+          <PostComponent />
           <CardTown />
         </div>
 
