3356887a397a24ee066cb0bd157275aa0d57ef73
CarolinaDelerue <delerue.carolina@gmail.com>
2022-06-30 17:12:06 +0200

    v3 nav

--- libside/src/components/Nav.jsx
+++ libside/src/components/Nav.jsx
@@ -8,6 +8,7 @@ import logo from "@assets/logo.png";
 const user = {
   name: "Tom Cook",
   email: "tom@example.com",
+  imageUrl: "./src/assets/icon_nav.png",
 };
 const navigation = [{ name: "Write an article", href: "#", current: true }];
 const userNavigation = [
@@ -22,7 +23,7 @@ function classNames(...classes) {
 
 export default function Example() {
   return (
-    <Disclosure as="nav" className="bg-red-800">
+    <Disclosure as="nav" className="bg-primary">
       {({ open }) => (
         <>
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -40,34 +41,12 @@ export default function Example() {
                   </Disclosure.Button>
                 </div>
                 <div className="flex-shrink-0 flex items-center">
-                  {/* <img
-                    className="block lg:hidden h-8 w-auto"
-                    src={logo}
-                    alt="Workflow"
-                  /> */}
                   <img
-                    className="  border border-red-400 lg:block h-12 w-12"
+                    className="  lg:block h-12 w-12"
                     src={logo}
                     alt="Workflow"
                   />
                 </div>
-                {/* <div className="hidden md:ml-6 md:flex md:items-center md:space-x-4">
-                  {navigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className={classNames(
-                        item.current
-                          ? "bg-gray-900 text-white"
-                          : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                        "px-3 py-2 rounded-md text-sm font-medium"
-                      )}
-                      aria-current={item.current ? "page" : undefined}
-                    >
-                      {item.name}
-                    </a>
-                  ))}
-                </div> */}
               </div>
               <div className="flex items-center">
                 <div className="flex-shrink-0">
@@ -83,54 +62,14 @@ export default function Example() {
                   </button>
                 </div>
                 <div className="hidden md:ml-4 md:flex-shrink-0 md:flex md:items-center">
-                  {/* Profile dropdown */}
                   <Menu as="div" className="ml-3 relative">
                     <div>
-                      <Menu.Button className=" flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                        <span className="sr-only">Open user menu</span>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          class="h-10 w-10"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                          stroke-width="2"
-                        >
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-                          />
-                        </svg>
-                      </Menu.Button>
+                      <img
+                        className="h-8 w-8 rounded-full bg-secondary "
+                        src={user.imageUrl}
+                        alt=""
+                      />
                     </div>
-                    <Transition
-                      as={Fragment}
-                      enter="transition ease-out duration-200"
-                      enterFrom="transform opacity-0 scale-95"
-                      enterTo="transform opacity-100 scale-100"
-                      leave="transition ease-in duration-75"
-                      leaveFrom="transform opacity-100 scale-100"
-                      leaveTo="transform opacity-0 scale-95"
-                    >
-                      <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        {userNavigation.map((item) => (
-                          <Menu.Item key={item.name}>
-                            {({ active }) => (
-                              <a
-                                href={item.href}
-                                className={classNames(
-                                  active ? "bg-gray-100" : "",
-                                  "block px-4 py-2 text-sm text-gray-700"
-                                )}
-                              >
-                                {item.name}
-                              </a>
-                            )}
-                          </Menu.Item>
-                        ))}
-                      </Menu.Items>
-                    </Transition>
                   </Menu>
                 </div>
               </div>
@@ -156,11 +95,11 @@ export default function Example() {
                 </Disclosure.Button>
               ))}
             </div>
-            <div className="pt-4 pb-3 border-t border-gray-700">
+            <div className="pt-4 pb-3 ">
               <div className="flex items-center px-5 sm:px-6">
-                <div className="flex-shrink-0">
+                <div className="flex-shrink-0 ">
                   <img
-                    className="h-10 w-10 rounded-full"
+                    className="h-8 w-8  rounded-full "
                     src={user.imageUrl}
                     alt=""
                   />
@@ -173,13 +112,6 @@ export default function Example() {
                     {user.email}
                   </div>
                 </div>
-                <button
-                  type="button"
-                  className="ml-auto flex-shrink-0 bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                >
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
               </div>
               <div className="mt-3 px-2 space-y-1 sm:px-3">
                 {userNavigation.map((item) => (
