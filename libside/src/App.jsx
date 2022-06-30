f05f9cf39c80dc290274f3a8ad2341096082b615
CarolinaDelerue <delerue.carolina@gmail.com>
2022-06-30 12:34:52 +0200

    V3 componentDetails

--- libside/src/App.jsx
+++ libside/src/App.jsx
@@ -2,11 +2,8 @@ import { Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage";
 import Post from "./pages/Post";
-<<<<<<< HEAD
 import ComponentDetails from "./pages/ComponentDetails";
-=======
 import ResearchPage from "./pages/ResearchPage";
->>>>>>> c3b4df27538ab979ab4c516d3d3f53b448672ab3
 
 function App() {
   return (
@@ -14,11 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/post" element={<Post />} />
-<<<<<<< HEAD
         <Route path="/component-details" element={<ComponentDetails />} />
-=======
         <Route path="/research" element={<ResearchPage />} />
->>>>>>> c3b4df27538ab979ab4c516d3d3f53b448672ab3
       </Routes>
     </div>
   );
