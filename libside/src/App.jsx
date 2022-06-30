fd8d30598a7acdf3f205b1d41db83094a51ddf51
Joffrey Chevalier <chevalier.joffrey@live.fr>
2022-06-30 10:19:39 +0200

    create SearchPage and set the route

--- libside/src/App.jsx
+++ libside/src/App.jsx
@@ -2,13 +2,15 @@ import { Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage";
 import Post from "./pages/Post";
+import ResearchPage from "./pages/ResearchPage";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/post" element={<Post />}/>
+        <Route path="/post" element={<Post />} />
+        <Route path="/research" element={<ResearchPage />} />
       </Routes>
     </div>
   );
---
+++ libside/src/pages/ResearchPage.jsx
@@ -0,0 +1,9 @@
+function ResearchPage({}) {
+  return (
+    <div>
+      <p></p>
+    </div>
+  );
+}
+
+export default ResearchPage;
